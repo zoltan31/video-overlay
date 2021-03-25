306786485fe61a583c1ef1fe027229b2dace63c6
roastfmla <623401@gmail.com>
2021-03-25 22:49:38 +0100

    stream
    indítani így kell: ng serve --proxy-config proxy.conf.json

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,12 +1,53 @@
 import * as express from "express";
+import { pathToFileURL } from "node:url";
 
 const app = express();
 const port = 5000;
+const fs = require("fs");
 
-app.get("/", (_request, response) => {
-	response.send("Hello World!");
-});
+
+
+app.get("/", function (req, res) {
+	res.sendFile("app.component.html", {root: "../app/src/app/"});
+  });
+
+app.get("/video", function (req, res) {
+	// Ensure there is a range given for the video
+	const range = req.headers.range;
+	if (!range) {
+	  res.status(400).send("Requires Range header");
+	}
+  
+	// get video stats (about 61MB)
+	const videoPath = "bigbuck.mp4";
+	const videoSize = fs.statSync("bigbuck.mp4").size;
+  
+	// Parse Range
+	// Example: "bytes=32324-"
+	const CHUNK_SIZE = 10 ** 6; // 1MB
+	const start = Number(range.replace(/\D/g, ""));
+	const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+  
+	// Create headers
+	const contentLength = end - start + 1;
+	const headers = {
+	  "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+	  "Accept-Ranges": "bytes",
+	  "Content-Length": contentLength,
+	  "Content-Type": "video/mp4",
+	};
+  
+	// HTTP Status 206 for Partial Content
+	res.writeHead(206, headers);
+  
+	// create video read stream for this particular chunk
+	const videoStream = fs.createReadStream(videoPath, { start, end });
+  
+	// Stream the video chunk to the client
+	videoStream.pipe(res);
+  });
 
 app.listen(port, () => {
 	console.log(`Server started at http://localhost:${port}`);
 });
+
