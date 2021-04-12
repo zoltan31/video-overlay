a3ff8381819376f052bcf22ffb26373db3e6245f
Reményi Gergely <remenyig@gmail.com>
2021-04-12 22:59:17 +0200

    Fix timer test

--- app/src/app/licit/timer/timer.component.spec.ts
+++ app/src/app/licit/timer/timer.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { SocketIoModule } from 'ngx-socket-io';
+import { socketConfig } from 'src/utils/socket-config';
 
 import { TimerComponent } from './timer.component';
 
@@ -8,6 +11,10 @@ describe('TimerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule,
+        SocketIoModule.forRoot(socketConfig)
+      ],
       declarations: [ TimerComponent ]
     })
     .compileComponents();
