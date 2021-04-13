939fd30c4268a191b82c1f603fe53c5ab8f28f5d
JuhaszPeter1337 <juhasz.peter1337@gmail.com>
2021-04-13 19:13:37 +0200

    Create frontend tests

--- app/src/app/licit/highest-bid/highest-bid.component.spec.ts
+++ app/src/app/licit/highest-bid/highest-bid.component.spec.ts
@@ -1,13 +1,28 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SocketIoModule } from 'ngx-socket-io';
+import { Observable, of } from 'rxjs';
 import { socketConfig } from 'src/utils/socket-config';
+import { AuctionService } from '../auction.service';
 
 import { HighestBidComponent } from './highest-bid.component';
+
+class AuctionStubService {
+  getAuction(): Observable<{price: number}> {
+    return of({
+      price: 100
+    });
+  }
+
+  getHighestBid(): Observable<number> {
+    return of(800)  
+  }
+}
 
 describe('HighestBidComponent', () => {
   let component: HighestBidComponent;
   let fixture: ComponentFixture<HighestBidComponent>;
+  let auctionServiceStub: AuctionStubService = new AuctionStubService();
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,7 +30,13 @@ describe('HighestBidComponent', () => {
         HttpClientModule,
         SocketIoModule.forRoot(socketConfig)
       ],
-      declarations: [ HighestBidComponent ]
+      declarations: [ HighestBidComponent ],
+      providers: [
+        {
+          provide: AuctionService,
+          useValue: auctionServiceStub
+        }
+      ]
     })
     .compileComponents();
   });
@@ -26,7 +47,11 @@ describe('HighestBidComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should be 800', () => {
+    expect(component.highestBid).toBe(800);
+  })
 });
--- app/src/app/licit/timer/timer.component.spec.ts
+++ app/src/app/licit/timer/timer.component.spec.ts
@@ -26,7 +26,7 @@ describe('TimerComponent', () => {
     fixture.detectChanges();
   });
 
-  // it('should create', () => {
-  //   expect(component).toBeTruthy();
-  // });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 });
